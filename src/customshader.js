276d5a4c630e934e0d2fe925cb35876d51bdbbb1
Anton Panchishin <anton.panchishin@uk.ey.com>
2023-09-18 12:36:15 -0400

    comment custom shader

--- src/customshader.js
+++ src/customshader.js
@@ -3,44 +3,46 @@
 export const OldScreenShader = {
 
 	uniforms: {
-
 		'tDiffuse': { value: null }
-
 	},
 
-	vertexShader: /* glsl */`
+	vertexShader: `
 
-		varying vec2 vUv;
+		varying vec2 v_uv;
 
 		void main() {
 
-			vUv = uv;
-
+			v_uv = uv;
 			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
 		}`,
 
-	fragmentShader: /* glsl */`
+	fragmentShader: `
 
 		#include <common>
-
         uniform sampler2D tDiffuse;
-
-		varying vec2 vUv;
+		varying vec2 v_uv;
 
 		void main() {
-            vec2 uv = vUv - 0.5;
-            vec2 distort;
-            distort.x = cos(uv.y*0.8);
-            distort.y = cos(uv.x*0.8);
-            uv /= distort;
+            float distortion = 0.8;
+            // center and distort uv have a curve
+            vec2 uv = v_uv - 0.5;
+            uv /= vec2(cos(uv.y*distortion),cos(uv.x*distortion));
+
+            // grey outside of regular viewing area
             if ((abs(uv.x) > 0.5) || (abs(uv.y) > 0.5)) {
                 gl_FragColor = vec4( vec3(0.01),1.0 );    
 
             } else {
+                // undo centering
                 uv += 0.5;
-                vec4 texel = texture2D( tDiffuse, vec2(uv.x,uv.y) );
+
+                // sample from distorted uv
+                vec4 texel = texture2D( tDiffuse, uv );
+
+                // add scanlines to give it an old-tv feel
                 texel += round(fract(uv.y*50.0))*0.003;
+
                 gl_FragColor = texel;
             }
 
